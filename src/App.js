b1ce066ca33e6265d62ec6e3a771347bbce165c4
akrobarth <akrobarth@gmail.com>
2020-02-16 23:33:59 +0100

    [MOD](core) setup experience

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component} from "react";
+import { hot } from 'react-hot-loader/root';
+import "./App.css";
+
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <Title> Hello, World!</Title>
+      </div>
+    );
+  }
+}
+
+export default hot(App);
